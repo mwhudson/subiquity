09351f014a4075bbcfa4420589a8a14f8b5d43d5
Michael Hudson-Doyle <michael.hudson@canonical.com>
2020-08-19 13:23:56 +1200

    add an api definition

---
+++ subiquity/common/api/defs.py
@@ -0,0 +1,31 @@
+# Copyright 2020 Canonical, Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def api(cls, prefix=''):
+    for k, v in cls.__dict__.items():
+        if isinstance(v, type):
+            v.__name__ = k
+            n = getattr(v, 'path', k)
+            v.fullpath = prefix + '/' + n
+            api(v, v.fullpath)
+    return cls
+
+
+def simple_endpoint(typ):
+    class endpoint:
+        def get() -> typ: pass
+        def post(data: typ): pass
+    return endpoint
