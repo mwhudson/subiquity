6b6ce966f74c46b694eee1bd743f918137cc1e51
Michael Hudson-Doyle <michael.hudson@canonical.com>
2021-05-28 15:34:04 +1200

    offer imsm volumes for guided storage, not its components

--- subiquity/server/controllers/filesystem.py
+++ subiquity/server/controllers/filesystem.py
@@ -51,6 +51,7 @@ from subiquity.common.types import (
     )
 from subiquity.models.filesystem import (
     dehumanize_size,
+    Raid,
     )
 from subiquity.server.controller import (
     SubiquityController,
@@ -222,14 +223,22 @@ class FilesystemController(SubiquityController, FilesystemManipulator):
             return probe_resp
         if not min_size:
             min_size = DEFAULT_MIN_SIZE_GUIDED
+        disks = []
+        for raid in self.model._all(type='raid'):
+            if not boot.can_be_boot_device(raid, with_reformatting=True):
+                continue
+            disks.append(raid)
+        for disk in self.model._all(type='disk'):
+            if not boot.can_be_boot_device(disk, with_reformatting=True):
+                continue
+            if isinstance(disk.constructed_device, Raid):
+                if disk.constructed_device._subvolumes:
+                    continue
+            disks.append(disk)
         return GuidedStorageResponse(
             status=ProbeStatus.DONE,
             error_report=self.full_probe_error(),
-            disks=[
-                labels.for_client(device, min_size=min_size)
-                for device in self.model._actions
-                if boot.can_be_boot_device(device, with_reformatting=True)
-            ])
+            disks=[labels.for_client(disk, min_size=min_size) for d in disks])
 
     async def guided_POST(self, choice: Optional[GuidedChoice]) \
             -> StorageResponse:
