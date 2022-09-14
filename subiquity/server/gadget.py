d96f4d2afedde12c02548192e0f0d43f9d2b2f3c
Michael Hudson-Doyle <michael.hudson@canonical.com>
2022-09-14 16:04:13 +1200

    add some snapd types

---
+++ subiquity/server/gadget.py
@@ -0,0 +1,54 @@
+# Copyright 2022 Canonical, Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import enum
+from typing import Optional
+
+import attr
+
+
+def named_field(name, default):
+    return attr.ib(metadata={'name': name}, default=default)
+
+
+class Role(enum.Enum):
+
+    MBR = 'mbr'
+    SYSTEM_BOOT = 'system-boot'
+    SYSTEM_BOOT_IMAGE = 'system-boot-image'
+    SYSTEM_BOOT_SELECT = 'system-boot-select'
+    SYSTEM_DATA = 'system-data'
+    SYSTEM_RECOVERY_SELECT = 'system-recovery-select'
+    SYSTEM_SAVE = 'system-save'
+    SYSTEM_SEED = 'system-seed'
+
+
+@attr.s(auto_attribs=True)
+class RelativeOffset:
+    relative_to: str = named_field('relative-to')
+    offset: int
+
+
+@attr.s(auto_attribs=True)
+class VolumeStructure:
+    name: str = ''
+    label: str = named_field('filesystem-label', '')
+    offset: Optional[int] = None
+    offset_write: Optional[RelativeOffset] = None
+    type: str = ''
+    role: Optional[Role] = None
+    id: Optional[str] = None
+    filesystem: str = ''
+    # content: List[VolumeContent] = attr.Factory(list)
